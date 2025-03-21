3c00ebf3be7156b6b12111c1610553f3de00b969
banjorabbit <kristiantimm@gmail.com>
2025-03-21 18:15:03 +0100

    fix include bug in helper.functions.h

--- include/algorithm_library/interface/helper_functions.h
+++ include/algorithm_library/interface/helper_functions.h
@@ -5,7 +5,7 @@
 // author: Kristian Timm Andersen
 
 // is x a positive power of two?
-bool isPositivePowerOfTwo(const int x) { return (x > 0) && ((x & (x - 1)) == 0); }
+static inline bool isPositivePowerOfTwo(const int x) { return (x > 0) && ((x & (x - 1)) == 0); }
 
 // number of bits required to represent x
-constexpr unsigned numberOfBits(unsigned x) { return x < 2 ? x : 1 + numberOfBits(x >> 1); }
+static inline constexpr unsigned numberOfBits(unsigned x) { return x < 2 ? x : 1 + numberOfBits(x >> 1); }
--- include/algorithm_library/interface/interface.h
+++ include/algorithm_library/interface/interface.h
@@ -3,12 +3,13 @@
     "algorithm_library/interface/get_dynamic_memory_size.h" //  member function added to Eigen DenseBase class to get dynamic memory size of array and matrices
 #define EIGEN_MPL2_ONLY                                     // don't allow LGPL licensed code from Eigen
 #include "algorithm_library/interface/constrained_type.h"   // ConstrainedType class
-#include "algorithm_library/interface/input_output.h"       // Input/Output structs.
-#include "algorithm_library/interface/macros_json.h"        // macros for coefficients and parameters
-#include "algorithm_library/interface/public_algorithm.h"   // Base Algorithm class
-#include "helper_functions.h"
-#include <Eigen/Dense> // Eigen Library.
+#include "algorithm_library/interface/helper_functions.h"
+#include "algorithm_library/interface/input_output.h"     // Input/Output structs.
+#include "algorithm_library/interface/macros_json.h"      // macros for coefficients and parameters
+#include "algorithm_library/interface/public_algorithm.h" // Base Algorithm class
+#include <Eigen/Dense>                                    // Eigen Library.
 #include <Eigen/IterativeLinearSolvers>
+
 
 // This is the main interface file that includes the necessary files for the public interface
 //
