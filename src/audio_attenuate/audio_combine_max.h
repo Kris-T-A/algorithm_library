40ed12c46d2093484f6eb0dc3f81b22e2c407512
thinkpad <kristiantimm@gmail.com>
2025-03-21 12:54:39 +0100

    add AudioComineMin

--- src/audio_attenuate/audio_combine_max.h
+++ src/audio_attenuate/audio_combine_max.h
@@ -3,12 +3,12 @@
 #include "filterbank_wola.h"
 #include "framework/framework.h"
 
-// Combine a number of audio channels to one channel by taking the maximum value of each frequency band. The input audio signal is a matrix of size bufferSize x nChannels and
-// the FFT size is 4 * bufferSize. The output audio signal is a vector of size bufferSize.
+// Combine a number of audio channels to one channel by taking the min/max power of each frequency band. The input audio signal is a matrix of size bufferSize x
+// nChannels and the FFT size is 4 * bufferSize. The output audio signal is a vector of size bufferSize.
 //
 // author: Kristian Timm Andersen
 
-struct AudioCombineMaxConfiguration
+struct AudioCombineConfiguration
 {
     using Input = I::Real2D;
     using Output = O::Real;
@@ -40,7 +40,7 @@ struct AudioCombineMaxConfiguration
     static bool validOutput(Output output, const Coefficients &c) { return output.allFinite() && (output.size() == c.bufferSize); }
 };
 
-class AudioCombineMax : public AlgorithmImplementation<AudioCombineMaxConfiguration, AudioCombineMax>
+class AudioCombineMax : public AlgorithmImplementation<AudioCombineConfiguration, AudioCombineMax>
 {
   public:
     AudioCombineMax(const Coefficients &c = Coefficients())
@@ -67,8 +67,60 @@ class AudioCombineMax : public AlgorithmImplementation<AudioCombineMaxConfigurat
         {
             for (auto i = 0; i < nBands; i++)
             {
-                float power = std::norm(spectrogramOut(i, channel));
+                const float power = std::norm(spectrogramOut(i, channel));
                 if (power > spectrogramPower(i))
+                {
+                    spectrogramPower(i) = power;
+                    spectrogramOut(i, 0) = spectrogramOut(i, channel);
+                }
+            }
+        }
+        filterbankSynthesis.process(spectrogramOut.col(0), output);
+    }
+
+    size_t getDynamicSizeVariables() const final
+    {
+        size_t size = spectrogramOut.getDynamicMemorySize();
+        size += spectrogramPower.getDynamicMemorySize();
+        return size;
+    }
+
+    int nBands;
+    Eigen::ArrayXXcf spectrogramOut;
+    Eigen::ArrayXf spectrogramPower;
+
+    friend BaseAlgorithm;
+};
+
+class AudioCombineMin : public AlgorithmImplementation<AudioCombineConfiguration, AudioCombineMin>
+{
+  public:
+    AudioCombineMin(const Coefficients &c = Coefficients())
+        : BaseAlgorithm{c},
+          filterbankAnalysis(
+              {.nChannels = c.nChannels, .bufferSize = c.bufferSize, .nBands = 2 * c.bufferSize + 1, .filterbankType = FilterbankAnalysisWOLA::Coefficients::HANN}),
+          filterbankSynthesis({.nChannels = 1, .bufferSize = c.bufferSize, .nBands = 2 * c.bufferSize + 1, .filterbankType = FilterbankSynthesisWOLA::Coefficients::HANN})
+    {
+        nBands = 2 * c.bufferSize + 1;
+        spectrogramOut.resize(nBands, c.nChannels);
+        spectrogramPower.resize(nBands);
+    }
+
+    FilterbankAnalysisWOLA filterbankAnalysis;
+    FilterbankSynthesisWOLA filterbankSynthesis;
+    DEFINE_MEMBER_ALGORITHMS(filterbankAnalysis, filterbankSynthesis)
+
+  private:
+    void processAlgorithm(Input input, Output output)
+    {
+        filterbankAnalysis.process(input, spectrogramOut);
+        spectrogramPower = spectrogramOut.col(0).abs2();
+        for (auto channel = 1; channel < C.nChannels; channel++)
+        {
+            for (auto i = 0; i < nBands; i++)
+            {
+                const float power = std::norm(spectrogramOut(i, channel));
+                if (power < spectrogramPower(i))
                 {
                     spectrogramPower(i) = power;
                     spectrogramOut(i, 0) = spectrogramOut(i, channel);
