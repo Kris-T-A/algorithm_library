567a96a2f8a9e06a1e844863a10d1651b3c751a1
banjorabbit <kristiantimm@gmail.com>
2025-07-21 21:38:59 +0200

    updated version of LogScale. Missing triangular interpolation

--- src/scale_transform/log_scale.h
+++ src/scale_transform/log_scale.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "algorithm_library/scale_transform.h"
 #include "framework/framework.h"
-
+#include "interpolation/interpolation_cubic.h"
+#include <iostream>
 // Logarithmic Scale
 //
 // author: Kristian Timm Andersen
@@ -11,83 +12,99 @@ class LogScale : public AlgorithmImplementation<ScaleTransformConfiguration, Log
   public:
     LogScale(Coefficients c = Coefficients()) : BaseAlgorithm{c}
     {
-        float lowFreqLog = static_cast<float>(c.indexEnd) / c.nInputs;
-        float highFreqLog = std::log10(1.f + c.indexEnd / lowFreqLog);
-        Eigen::ArrayXf linLogs = Eigen::ArrayXf::LinSpaced(c.nOutputs + 1, 0, highFreqLog);
-        Eigen::ArrayXf freqs = lowFreqLog * (Eigen::ArrayXf::Constant(c.nOutputs + 1, 10).pow(linLogs) - 1.f);
-        Eigen::ArrayXf cornerBinsFloat = ((c.nInputs - 1) / c.indexEnd * freqs);
+        float minFreq = 20;                                                  // minimum corner frequency in Hz
+        float freqPerBin = static_cast<float>(c.indexEnd) / (c.nInputs - 1); // frequency difference between two adjacent bins
+        // linear scale from corresponding minFreq to log10(nInputs)
+        Eigen::ArrayXf linLogs = Eigen::ArrayXf::LinSpaced(c.nOutputs, std::log10(1.f + minFreq / freqPerBin), std::log10(static_cast<float>(c.nInputs)));
+        // logarithmic scale from 0 to nInputs-1. Corresponds to index of corner bins in the input array (size: nOutputs)
+        Eigen::ArrayXf centerBins = (Eigen::ArrayXf::Constant(c.nOutputs, 10).pow(linLogs) - 1.f);
+        Eigen::ArrayXf freqs = freqPerBin * centerBins; // convert center bins to frequencies
 
-        Eigen::ArrayXf cornerBinsFloatDiff = cornerBinsFloat.tail(c.nOutputs) - cornerBinsFloat.head(c.nOutputs);
-        nSmallBins = (cornerBinsFloatDiff <= 1.f).count();                                        // number of bins that has width smaller or equal to 1
-        binsWeight = cornerBinsFloat.head(nSmallBins) - cornerBinsFloat.head(nSmallBins).floor(); // weight for the bins with width smaller or equal to 1
+        std::cout << "freqs: " << freqs.transpose() << std::endl;
+        std::cout << "freqs.size(): " << freqs.size() << std::endl;
+        std::cout << "centerBins: " << centerBins.transpose() << std::endl;
+        std::cout << "centerBins.size(): " << centerBins.size() << std::endl;
 
-        Eigen::ArrayXi cornerBins =
-            cornerBinsFloat.round().cast<int>(); // corner bins, including 0 and nInputs. Use round() instead of floor() to ensure increasing mel bin sizes
-        indexStart.resize(c.nOutputs);
-        indexStart.head(nSmallBins) = cornerBinsFloat.head(nSmallBins).floor().cast<int>();
-        indexStart.tail(c.nOutputs - nSmallBins) = cornerBins.segment(nSmallBins, c.nOutputs - nSmallBins);
-        nInputsSum.resize(c.nOutputs);
-        nInputsSum.head(nSmallBins) = Eigen::ArrayXi::Constant(nSmallBins, 1);
-        nInputsSum.tail(c.nOutputs - nSmallBins) =
-            (cornerBins.segment(nSmallBins + 1, c.nOutputs - nSmallBins) - indexStart.segment(nSmallBins, c.nOutputs - nSmallBins)).cwiseMax(1);
-        indexEnd = indexStart(c.nOutputs - 1) + nInputsSum(c.nOutputs - 1);
-    }
-
-    inline void inverse(I::Real2D xPower, O::Real2D yPower)
-    {
-        assert(xPower.rows() == C.nOutputs);
-
-        for (auto channel = 0; channel < xPower.cols(); channel++)
+        // count number of output bins that has width smaller or equal to 1 (corresponds to upsampling)
+        // these output bins will be calculated using linear interpolation
+        nLinearBins = 0; // number of bins calculated using linear interpolation
+        while (((centerBins(nLinearBins + 1) - centerBins(nLinearBins) <= 1.f) && (nLinearBins < c.nOutputs - 1)) || (centerBins(nLinearBins) < 1.f))
         {
-            for (auto i = 0; i < C.nOutputs; i++)
-            {
-                yPower.block(indexStart(i), channel, nInputsSum(i), 1).setConstant(xPower(i, channel));
-            }
-            yPower.block(indexEnd, channel, C.nInputs - indexEnd, 1).setConstant(xPower(C.nOutputs - 1, channel));
+            nLinearBins++;
         }
+        fractionLinear = centerBins.head(nLinearBins) - centerBins.head(nLinearBins).floor(); // fraction for linear interpolation
+        indexStart = centerBins.head(nLinearBins).cast<int>();
+
+        // count number of output bins that has width smaller or equal to 2 (corresponds to downsampling by a factor of 2 or less)
+        // these output bins will be calculated using cubic interpolation
+        int nSum = nLinearBins; // number of bins calculated using linear and cubic interpolation
+        while (((centerBins(nSum + 1) - centerBins(nSum) <= 2.f) || (centerBins(nSum) < 2.f)) && (nSum < c.nOutputs - 2))
+        {
+            nSum++;
+        }
+        nCubicBins = nSum - nLinearBins; // number of cubic bins
+        fractionCubic = centerBins.segment(nLinearBins, nCubicBins);
     }
 
-    // Indices corresponds to frequencies if indexEnd is half the sample rate
-    Eigen::ArrayXf getCornerIndices() const
-    {
-        Eigen::ArrayXf array(C.nOutputs + 1);
-        array.head(nSmallBins) = (1 - binsWeight) * indexStart.head(nSmallBins).cast<float>() + binsWeight * (indexStart.head(nSmallBins) + 1).cast<float>();
-        array.segment(nSmallBins, C.nOutputs - nSmallBins) = indexStart.segment(nSmallBins, C.nOutputs - nSmallBins).cast<float>();
-        array.tail(1) = static_cast<float>(indexEnd);
-        array *= C.indexEnd / (C.nInputs - 1);
-        return array;
-    }
+    InterpolationCubic interpolationCubic;
+    DEFINE_MEMBER_ALGORITHMS(interpolationCubic)
+
+    // inline void inverse(I::Real2D xPower, O::Real2D yPower)
+    // {
+    //     assert(xPower.rows() == C.nOutputs);
+
+    //     for (auto channel = 0; channel < xPower.cols(); channel++)
+    //     {
+    //         for (auto i = 0; i < C.nOutputs; i++)
+    //         {
+    //             yPower.block(indexStart(i), channel, nInputsSum(i), 1).setConstant(xPower(i, channel));
+    //         }
+    //         yPower.block(indexEnd, channel, C.nInputs - indexEnd, 1).setConstant(xPower(C.nOutputs - 1, channel));
+    //     }
+    // }
+
+    // // Indices corresponds to frequencies if indexEnd is half the sample rate
+    // Eigen::ArrayXf getCornerIndices() const
+    // {
+    //     Eigen::ArrayXf array(C.nOutputs + 1);
+    //     array.head(nLinearBins) = (1 - binsWeight) * indexStart.head(nLinearBins).cast<float>() + binsWeight * (indexStart.head(nLinearBins) + 1).cast<float>();
+    //     array.segment(nLinearBins, C.nOutputs - nLinearBins) = indexStart.segment(nLinearBins, C.nOutputs - nLinearBins).cast<float>();
+    //     array.tail(1) = static_cast<float>(indexEnd);
+    //     array *= C.indexEnd / (C.nInputs - 1);
+    //     return array;
+    // }
 
   private:
     inline void processAlgorithm(Input input, Output output)
     {
         for (auto channel = 0; channel < input.cols(); channel++)
         {
-            for (auto i = 0; i < nSmallBins; i++)
+            // linear interpolation
+            for (auto i = 0; i < nLinearBins; i++)
             {
                 // weighted sum
-                output(i, channel) = (1.f - binsWeight(i)) * input(indexStart(i), channel) + binsWeight(i) * input(indexStart(i) + 1, channel);
+                output(i, channel) = (1.f - fractionLinear(i)) * input(indexStart(i), channel) + fractionLinear(i) * input(indexStart(i) + 1, channel);
             }
-            for (auto i = nSmallBins; i < C.nOutputs; i++)
-            {
-                // max
-                output(i, channel) = input.block(indexStart(i), channel, nInputsSum(i), 1).maxCoeff();
-            }
+
+            // cubic interpolation
+            interpolationCubic.process({input.col(channel), fractionCubic}, output.col(channel).segment(nLinearBins, nCubicBins));
+
+            // triangular interpolation
         }
     }
 
     size_t getDynamicSizeVariables() const final
     {
         size_t size = indexStart.getDynamicMemorySize();
-        size += nInputsSum.getDynamicMemorySize();
-        size += binsWeight.getDynamicMemorySize();
+        size += fractionLinear.getDynamicMemorySize();
         return size;
     }
 
-    int indexEnd, nSmallBins;
+  public:
+    int nLinearBins, nCubicBins;
     Eigen::ArrayXi indexStart;
-    Eigen::ArrayXi nInputsSum;
-    Eigen::ArrayXf binsWeight;
+    Eigen::ArrayXf fractionLinear;
+    Eigen::ArrayXf fractionCubic;
 
     friend BaseAlgorithm;
 };
