df301c133814bc7256ef8804da676beac75174fc
Kris-T-A <kristiantimm@gmail.com>
2026-04-27 18:04:24 +0200

    spectrogram_adaptive: dispatch on method (ZEROPAD/MOVING) via setImplementation

--- src/spectrogram_adaptive/spectrogram_adaptive.cpp
+++ src/spectrogram_adaptive/spectrogram_adaptive.cpp
@@ -1,3 +1,20 @@
+#include "spectrogram_adaptive/spectrogram_adaptive_moving.h"
 #include "spectrogram_adaptive/spectrogram_adaptive_zeropad.h"
 
-DEFINE_ALGORITHM_CONSTRUCTOR(SpectrogramAdaptive, SpectrogramAdaptiveZeropad, SpectrogramAdaptiveConfiguration)
+using ZeropadImpl = Implementation<SpectrogramAdaptiveZeropad, SpectrogramAdaptiveConfiguration>;
+using MovingImpl = Implementation<SpectrogramAdaptiveMoving, SpectrogramAdaptiveConfiguration>;
+
+template <>
+void Algorithm<SpectrogramAdaptiveConfiguration>::setImplementation(const Coefficients &c)
+{
+    if (c.method == SpectrogramAdaptiveConfiguration::Coefficients::ZEROPAD)
+    {
+        pimpl = std::make_unique<ZeropadImpl>(c);
+    }
+    else
+    {
+        pimpl = std::make_unique<MovingImpl>(c);
+    }
+}
+
+SpectrogramAdaptive::SpectrogramAdaptive(const Coefficients &c) : Algorithm<SpectrogramAdaptiveConfiguration>(c) {}
